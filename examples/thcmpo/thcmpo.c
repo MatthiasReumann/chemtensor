96a0e5abda2757a2fada230463b547e7d2a5a445
Matthias Reumann <matthias.reumann@tum.de>
2024-10-24 11:05:49 +0200

    Add G dictionary

--- examples/thcmpo/thcmpo.c
+++ examples/thcmpo/thcmpo.c
@@ -2,74 +2,45 @@
 #include "aligned_memory.h"
 #include "mpo.h"
 
-// TODO: Ask Yu/Prof.Mendl if I can implement this using the opchain method. Problem: Bosonic Z.
-//
-//
-void construct_elementary_thc_mpo(const int nsites, const dcomplex *chi_row, struct mpo_assembly *assembly)
+
+struct g_pair {
+    struct mpo p;
+    struct mpo q;
+};
+
+
+struct g {
+    struct g_pair* fake_dict;  // List of MPO pairs accessible like an dictionary with tuple key via get_value(...)
+    long N;               // THC Rank `N`. Length of `fake_dict` is `2N`.
+};
+
+
+void allocate_g(struct g* g, const long N)
 {
-    assert(nsites >= 2);
-
-    // Setup physical quantum numbers.
-    assembly->d = 2;
-    assembly->dtype = CT_DOUBLE_COMPLEX;
-    assembly->qsite = ct_calloc(assembly->d, sizeof(qnumber));
-    assembly->qsite[1] = 1;
-
-    // operator map
-    // 0:I, 1:Z, 2:chi_0*a, 3:chi_1*a, ...
-    assembly->num_local_ops = 2 + nsites; // Identity + Z + each site ann/crea op.
-    assembly->opmap = ct_malloc(assembly->num_local_ops * sizeof(struct dense_tensor));
-    for (int i = 0; i < assembly->num_local_ops; i++)
-    {
-        const long dim[2] = {assembly->d, assembly->d};
-        allocate_dense_tensor(assembly->dtype, 2, dim, &assembly->opmap[i]);
-    }
-
-    const dcomplex pZ[4] = {1, 0, 0, -1};
-    const dcomplex annihilation[4] = {0, 1, 0, 0};
-    const dcomplex creation[4] = {0, 0, 1, 0};
-
-    dense_tensor_set_identity(&assembly->opmap[0]);  // copy 'I'
-    memcpy(assembly->opmap[2].data, pZ, sizeof(pZ)); // copy 'Z'
-    for (int i = 2; i < nsites + 2; i++)
-    { // copy 'a'
-        memcpy(assembly->opmap[i].data, creation, sizeof(creation));
-    }
-
-    // coefficient map; first two entries must always be 0 and 1
-    assembly->num_coeffs = 2 + nsites;
-    assembly->coeffmap = (dcomplex *)ct_malloc(assembly->num_coeffs * sizeof(dcomplex));
-
-    const double coeffmap[2] = {0, 1};
-    memcpy(assembly->coeffmap, coeffmap, sizeof(2 * sizeof(dcomplex)));         // copy mandatory
-    memcpy(assembly->coeffmap + 2, chi_row, sizeof(nsites * sizeof(dcomplex))); // copy prefactors
-
-    // // local two-site and single-site terms
-    // int oids_c0[] = { OID_Z, OID_Z };  qnumber qnums_c0[] = { 0, 0, 0 };
-    // int oids_c1[] = { OID_Z };         qnumber qnums_c1[] = { 0, 0 };
-    // int oids_c2[] = { OID_X };         qnumber qnums_c2[] = { 0, 0 };
-    // struct op_chain lopchains[] = {
-    // 	{ .oids = oids_c0, .qnums = qnums_c0, .cid = 2, .length = ARRLEN(oids_c0), .istart = 0 },
-    // 	{ .oids = oids_c1, .qnums = qnums_c1, .cid = 3, .length = ARRLEN(oids_c1), .istart = 0 },
-    // 	{ .oids = oids_c2, .qnums = qnums_c2, .cid = 4, .length = ARRLEN(oids_c2), .istart = 0 },
-    // };
-
-    // // convert to an MPO graph
-    // local_opchains_to_mpo_graph(nsites, lopchains, ARRLEN(lopchains), &assembly->graph);
+    assert(N > 0);
+    g->N = N;
+    g->fake_dict = ct_malloc(2 * N * sizeof(struct g_pair));
 }
 
 
-void construct_elementary_mpo(const int nsites, const dcomplex* chi_row, const bool is_creation, struct mpo* mpo)
+struct g_pair* get_value(const struct g* g, const int i, const int s)
+{
+    assert(i + g->N * s < 2 * g->N);
+    return &(g->fake_dict[i + g->N * s]);
+}
+
+
+void construct_elementary_mpo(const int L, const dcomplex* chi_row, const bool is_creation, struct mpo* mpo)
 {
     const long d = 2;
     const qnumber qd[2] = {0, 1};
 
     mpo->d = d;
-    mpo->nsites = nsites;
+    mpo->nsites = L;
     mpo->qsite = ct_malloc(d * sizeof(qnumber));
     memcpy(mpo->qsite, &qd, d * sizeof(qnumber));
 
-    mpo->a = ct_calloc(nsites, sizeof(struct block_sparse_tensor));
+    mpo->a = ct_calloc(L, sizeof(struct block_sparse_tensor));
     
     const enum tensor_axis_direction axis_dir[4] = { TENSOR_AXIS_OUT, TENSOR_AXIS_OUT, TENSOR_AXIS_IN, TENSOR_AXIS_IN };
     
@@ -100,7 +71,7 @@ void construct_elementary_mpo(const int nsites, const dcomplex* chi_row, const b
     }
 
     // intermediate sites: dimensions: 2 x d x d x 2
-    for (int i = 1; i < nsites - 1; i++)
+    for (int i = 1; i < L - 1; i++)
 	{
         const long dim[4] = { 2, d, d, 2 };
         allocate_dense_tensor(CT_DOUBLE_COMPLEX, 4, dim, &dt);
@@ -131,25 +102,61 @@ void construct_elementary_mpo(const int nsites, const dcomplex* chi_row, const b
             const qnumber qD[2] = { 0, 1 };
             const qnumber qD_next[1] = { 1 };
             const qnumber* qnums[4] = { qD, qd, qd, qD_next};
-            const dcomplex data[8] = { 0, 0, chi_row[nsites - 1], 0, 1, 0, 0, 1};
+            const dcomplex data[8] = { 0, 0, chi_row[L - 1], 0, 1, 0, 0, 1};
 
             memcpy(dt.data, data, 8 * sizeof(dcomplex));
-            dense_to_block_sparse_tensor(&dt, axis_dir, qnums, &mpo->a[nsites - 1]);
+            dense_to_block_sparse_tensor(&dt, axis_dir, qnums, &mpo->a[L - 1]);
         } else {
             const qnumber qD[2] = { 0, -1 };
             const qnumber qD_next[1] = { -1 };
             const qnumber* qnums[4] = { qD, qd, qd, qD_next};
-            const dcomplex data[8] = { 0, chi_row[nsites - 1], 0, 0, 1, 0, 0, 1};
+            const dcomplex data[8] = { 0, chi_row[L - 1], 0, 0, 1, 0, 0, 1};
 
             memcpy(dt.data, data, 8 * sizeof(dcomplex));
-            dense_to_block_sparse_tensor(&dt, axis_dir, qnums, &mpo->a[nsites - 1]);
+            dense_to_block_sparse_tensor(&dt, axis_dir, qnums, &mpo->a[L - 1]);
         }
     }
 }
 
+
+void interleave_zero(const dcomplex* a, const long n, const long offset, dcomplex** ret)
+{
+    *ret = ct_calloc(2 * n, sizeof(dcomplex));
+    for (int i = 0; i < n; i++) {
+        (*ret)[offset + 2 * i] = a[i];
+    }
+}
+
+
+void construct_g(dcomplex** chi, const long N, const long L, struct g* g){
+    
+    // chi.shape = (N, L/2)
+    dcomplex* chi_row;   
+    
+    allocate_g(g, N);
+    for (int i = 0; i < N; i++) {
+        // spin up
+        {
+            struct g_pair* pair = get_value(g, i, 0);   
+            interleave_zero(chi[i], L / 2, 0, &chi_row);
+            construct_elementary_mpo(L, chi_row, false, &pair->p);
+            construct_elementary_mpo(L, chi_row, true, &pair->q);
+        }
+
+        // spin down
+        {
+            struct g_pair* pair = get_value(g, i, 1);
+            interleave_zero(chi[i], L / 2, 1, &chi_row);
+            construct_elementary_mpo(L, chi_row, false, &pair->p);
+            construct_elementary_mpo(L, chi_row, true, &pair->q);
+        }
+    }
+}
+
+
 void print_mpo(struct mpo mpo)
 {
-    printf("MPO: nsites=%d, d=%ld, qsite=(%d, %d)\n", mpo.nsites, mpo.d, mpo.qsite[0], mpo.qsite[1]);
+    printf("MPO: L=%d, d=%ld, qsite=(%d, %d)\n", mpo.nsites, mpo.d, mpo.qsite[0], mpo.qsite[1]);
 
     for(int i = 0; i < mpo.nsites; i++) {
         struct dense_tensor dt;
@@ -169,10 +176,39 @@ void print_mpo(struct mpo mpo)
 
 int main()
 {
-    struct mpo mpo;
-    const int nsites = 3;
-    const dcomplex chi_row[3] = { 42, 1337, 72 };
-    construct_elementary_mpo(nsites, chi_row, false, &mpo);
-    print_mpo(mpo);
-    printf("is_conssitent=%d\n", mpo_is_consistent(&mpo));
+    const long N = 4;
+    const long L = 6;
+
+    const dcomplex lst[N][L/2] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+        {10, 11, 12}
+    };
+
+    dcomplex** chi = ct_malloc(N * sizeof(dcomplex*));
+    for (int i = 0; i < N; i++){
+        chi[i] = ct_malloc(L/2 * sizeof(dcomplex));
+        for (int j = 0; j < L/2; j++) {
+            chi[i][j] = lst[i][j];
+        }
+    }
+    
+    struct g g;
+    construct_g(chi, N, L, &g);
+    struct g_pair* pair_ret = get_value(&g, 3, 1);
+    print_mpo(pair_ret->p);
+    print_mpo(pair_ret->q);
+
+    // struct mpo p, q;
+    // const int nsites = 3;
+    // const dcomplex chi_row[3] = { 42, 1337, 72 };
+    // construct_elementary_mpo(nsites, chi_row, true, &p);
+    // construct_elementary_mpo(nsites, chi_row, false, &q);
+    // allocate_g(&g, 7);
+    // struct g_pair pair = { .p = &p, .q = &q };
+    // set_value(&g, 0, 0, &pair);
+    // struct g_pair* pair_ret = get_value(&g, 0, 0);
+    // print_mpo(*pair_ret->p);
+    // print_mpo(*pair_ret->q);
 }
