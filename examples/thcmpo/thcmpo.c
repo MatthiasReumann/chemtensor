72d1d6b407fd2b62ae7a118abc8fec7347f5e237
Matthias Reumann <matthias.reumann@tum.de>
2024-10-25 09:44:05 +0200

    Add 'construct_computational_basis_mps' function

--- examples/thcmpo/thcmpo.c
+++ examples/thcmpo/thcmpo.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
-#include "aligned_memory.h"
+#include "mps.h"
 #include "mpo.h"
+#include "operation.h"
+#include "aligned_memory.h"
 
+
+void THCMPO_print_mpo(struct mpo mpo)
+{
+    printf("MPO: L=%d, d=%ld, qsite=(%d, %d)\n", mpo.nsites, mpo.d, mpo.qsite[0], mpo.qsite[1]);
+
+    for(int i = 0; i < mpo.nsites; i++) {
+        struct dense_tensor dt;
+        block_sparse_to_dense_tensor(&mpo.a[i], &dt);
+
+        dcomplex* data = (dcomplex*)(dt.data);
+        const long nblocks = integer_product(dt.dim, dt.ndim);
+        printf("[");
+        for (long k = 0; k < nblocks; k++)
+		{
+			printf(" %.2f", creal(data[k]));
+		}
+        printf("]\n");
+    }
+}
+
+
+void THCMPO_print_bst(struct block_sparse_tensor bst)
+{
+    printf("bst=[ndim=%d, dim=(%ld, %ld, %ld)]\n", 
+            bst.ndim, 
+            bst.dim_blocks[0], bst.dim_blocks[1], bst.dim_blocks[2]);
+}
+
+
+void THCMPO_print_dt(struct dense_tensor dt)
+{
+    printf("dt=[ndim=%d, dim=(%ld, %ld, %ld)]\n",
+        dt.ndim,
+        dt.dim[0],dt.dim[1],dt.dim[2]);
+}
 
 struct g_pair {
     struct mpo p;
@@ -10,12 +47,12 @@ struct g_pair {
 
 
 struct g {
-    struct g_pair* fake_dict;  // List of MPO pairs accessible like an dictionary with tuple key via get_value(...)
-    long N;               // THC Rank `N`. Length of `fake_dict` is `2N`.
+    struct g_pair* fake_dict;   // List of MPO pairs accessible like an dictionary with tuple key via get_value(...)
+    long N;                     // THC Rank `N`. Length of `fake_dict` is `2N`.
 };
 
 
-void allocate_g(struct g* g, const long N)
+void THCMPO_g_allocate(struct g* g, const long N)
 {
     assert(N > 0);
     g->N = N;
@@ -23,14 +60,14 @@ void allocate_g(struct g* g, const long N)
 }
 
 
-struct g_pair* get_value(const struct g* g, const int i, const int s)
+void THCMPO_g_get(const struct g* g, const int i, const int s, struct g_pair** g_pair)
 {
     assert(i + g->N * s < 2 * g->N);
-    return &(g->fake_dict[i + g->N * s]);
+    *g_pair = &(g->fake_dict[i + g->N * s]);
 }
 
 
-void construct_elementary_mpo(const int L, const dcomplex* chi_row, const bool is_creation, struct mpo* mpo)
+void THCMPO_construct_elementary_mpo(const int L, const dcomplex* chi_row, const bool is_creation, struct mpo* mpo)
 {
     const long d = 2;
     const qnumber qd[2] = {0, 1};
@@ -71,7 +108,7 @@ void construct_elementary_mpo(const int L, const dcomplex* chi_row, const bool i
     }
 
     // intermediate sites: dimensions: 2 x d x d x 2
-    for (int i = 1; i < L - 1; i++)
+    for (long i = 1; i < L - 1; i++)
 	{
         const long dim[4] = { 2, d, d, 2 };
         allocate_dense_tensor(CT_DOUBLE_COMPLEX, 4, dim, &dt);
@@ -119,65 +156,132 @@ void construct_elementary_mpo(const int L, const dcomplex* chi_row, const bool i
 }
 
 
-void interleave_zero(const dcomplex* a, const long n, const long offset, dcomplex** ret)
+void THCMPO_interleave_zero(const dcomplex* a, const long n, const long offset, dcomplex** ret)
 {
     *ret = ct_calloc(2 * n, sizeof(dcomplex));
-    for (int i = 0; i < n; i++) {
+    for (long i = 0; i < n; i++) {
         (*ret)[offset + 2 * i] = a[i];
     }
 }
 
 
-void construct_g(dcomplex** chi, const long N, const long L, struct g* g){
+void THCMPO_g_construct_from_chi(dcomplex** chi, const long N, const long L, struct g* g){
     
     // chi.shape = (N, L/2)
-    dcomplex* chi_row;   
+    dcomplex* chi_row;
     
-    allocate_g(g, N);
-    for (int i = 0; i < N; i++) {
+    THCMPO_g_allocate(g, N);
+    for (long i = 0; i < N; i++) {
         // spin up
         {
-            struct g_pair* pair = get_value(g, i, 0);   
-            interleave_zero(chi[i], L / 2, 0, &chi_row);
-            construct_elementary_mpo(L, chi_row, false, &pair->p);
-            construct_elementary_mpo(L, chi_row, true, &pair->q);
+            struct g_pair* pair;
+            THCMPO_g_get(g, i, 0, &pair);   
+            THCMPO_interleave_zero(chi[i], L / 2, 0, &chi_row);
+            THCMPO_construct_elementary_mpo(L, chi_row, false, &pair->p);
+            THCMPO_construct_elementary_mpo(L, chi_row, true, &pair->q);
         }
 
         // spin down
         {
-            struct g_pair* pair = get_value(g, i, 1);
-            interleave_zero(chi[i], L / 2, 1, &chi_row);
-            construct_elementary_mpo(L, chi_row, false, &pair->p);
-            construct_elementary_mpo(L, chi_row, true, &pair->q);
+            struct g_pair* pair;
+            THCMPO_g_get(g, i, 1, &pair);
+            THCMPO_interleave_zero(chi[i], L / 2, 1, &chi_row);
+            THCMPO_construct_elementary_mpo(L, chi_row, false, &pair->p);
+            THCMPO_construct_elementary_mpo(L, chi_row, true, &pair->q);
         }
     }
 }
 
 
-void print_mpo(struct mpo mpo)
+void THCMPO_calculate_phi(const struct mps *psi, struct g* g, const long N, const double tol, struct mps* phi)
 {
-    printf("MPO: L=%d, d=%ld, qsite=(%d, %d)\n", mpo.nsites, mpo.d, mpo.qsite[0], mpo.qsite[1]);
+    const int S = 2; // |{UP, DOWN}| = 2
+    for(long n = 0; n < N; n++) {
+        for(int s1 = 0; s1 < S; s1++) {
+            
+            struct mps a;
+            struct mps b;
+            struct g_pair* pair_n_s1;
+            THCMPO_g_get(g, n, s1, &pair_n_s1);
 
-    for(int i = 0; i < mpo.nsites; i++) {
-        struct dense_tensor dt;
-        block_sparse_to_dense_tensor(&mpo.a[i], &dt);
 
-        dcomplex* data = (dcomplex*)(dt.data);
-        const long nblocks = integer_product(dt.dim, dt.ndim);
-        printf("[");
-        for (long k = 0; k < nblocks; k++)
-		{
-			printf(" %.2f", creal(data[k]));
-		}
-        printf("]\n");
+            apply_operator(&pair_n_s1->p, psi, &a);
+            // TODO: Compress
+            apply_operator(&pair_n_s1->q, &a, &b);
+            // TODO: Compress
+
+            for(long m = 0; m < N; m++) {
+                for(int s2 = 0; s2 < S; s2++) {
+                    struct mps c;
+                    struct mps d;
+                    struct g_pair* pair_m_s2;
+                    THCMPO_g_get(g, m, s2, &pair_m_s2);
+
+                    apply_operator(&pair_m_s2->p, &b, &c);
+                    // TODO: Compress
+                    apply_operator(&pair_m_s2->q, &c, &d);
+                    // TODO: Compress
+                }
+            }
+        }
     }
 }
 
 
+void construct_computational_basis_mps(
+    const enum numeric_type dtype,
+    const int nsites,
+    const int basis_state,
+    struct mps* mps
+) 
+{
+    const long d = 2;
+    const qnumber qsite[2] = { 0, 1 };
+    
+    const dcomplex state_zero[2] = { 1, 0 };
+    const dcomplex state_one[2] = { 0, 1 };
+
+    const int ndim = 3;
+	const long dim[3] = { 1, d, 1 };
+
+    const enum tensor_axis_direction axis_dir[3] = { 
+        TENSOR_AXIS_OUT, TENSOR_AXIS_OUT, TENSOR_AXIS_IN
+    };
+
+    int acc = 0;
+    
+    qnumber qbond_curr[1] = { acc };
+    qnumber qbond_next[1];
+
+    allocate_empty_mps(nsites, d, qsite, mps);
+    
+    for (int i = 0; i < nsites; i++) {        
+        const int ith = ((basis_state & (1 << (nsites - i - 1))) >> (nsites - i - 1));
+
+        struct dense_tensor dt;
+        allocate_dense_tensor(dtype, ndim, dim, &dt);
+    
+        if (ith == 0) {
+            memcpy(dt.data, &state_zero, 2 * sizeof(dcomplex));
+        } else if (ith == 1) {
+            memcpy(dt.data, &state_one, 2 * sizeof(dcomplex));
+            acc++;
+        }
+        qbond_next[0] = acc;
+        
+        const qnumber* qnums[3] = { qbond_curr, qsite, qbond_next };
+        dense_to_block_sparse_tensor(&dt, axis_dir, qnums, &mps->a[i]);
+
+        delete_dense_tensor(&dt);
+
+        qbond_curr[0] = qbond_next[0];
+	}
+}
+
 int main()
 {
-    const long N = 4;
-    const long L = 6;
+    const long N = 28;
+    const long L = 14;
 
     const dcomplex lst[N][L/2] = {
         {1, 2, 3},
@@ -190,25 +294,21 @@ int main()
     for (int i = 0; i < N; i++){
         chi[i] = ct_malloc(L/2 * sizeof(dcomplex));
         for (int j = 0; j < L/2; j++) {
-            chi[i][j] = lst[i][j];
+            chi[i][j] = 1.3f;
         }
     }
     
     struct g g;
-    construct_g(chi, N, L, &g);
-    struct g_pair* pair_ret = get_value(&g, 3, 1);
-    print_mpo(pair_ret->p);
-    print_mpo(pair_ret->q);
+    struct mps psi;
+    struct mps phi;
+    THCMPO_g_construct_from_chi(chi, N, L, &g);
+    construct_computational_basis_mps(CT_DOUBLE_COMPLEX, L, 0b11111111110000, &psi);
 
-    // struct mpo p, q;
-    // const int nsites = 3;
-    // const dcomplex chi_row[3] = { 42, 1337, 72 };
-    // construct_elementary_mpo(nsites, chi_row, true, &p);
-    // construct_elementary_mpo(nsites, chi_row, false, &q);
-    // allocate_g(&g, 7);
-    // struct g_pair pair = { .p = &p, .q = &q };
-    // set_value(&g, 0, 0, &pair);
-    // struct g_pair* pair_ret = get_value(&g, 0, 0);
-    // print_mpo(*pair_ret->p);
-    // print_mpo(*pair_ret->q);
+    struct dense_tensor dt;
+    struct block_sparse_tensor bst;
+    mps_to_statevector(&psi, &bst);
+
+    block_sparse_to_dense_tensor(&bst, &dt);
+
+    THCMPO_print_dt(dt);
 }
