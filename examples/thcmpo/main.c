704bf8982e200e6592cf3b3ffdf2489ef6807722
Matthias Reumann <matthias.reumann@tum.de>
2024-12-06 08:40:27 +0100

    Update main.c

--- examples/thcmpo/main.c
+++ examples/thcmpo/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <time.h>
-#include <omp.h>
 #include "mps.h"
 #include "mpo.h"
 #include "hamiltonian.h"
@@ -8,53 +7,6 @@
 #include "utils.h"
 #include "states.h"
 #include "thcmpo.h"
-
-void read_data(double *zeta, double *chi)
-{
-    hid_t file = H5Fopen("../examples/thcmpo/data/water_chi_zeta.h5", H5F_ACC_RDONLY, H5P_DEFAULT);
-    if (file < 0)
-    {
-        printf("'H5Fopen' failed\n");
-    }
-
-    if (read_hdf5_dataset(file, "zeta", H5T_NATIVE_DOUBLE, zeta) < 0)
-    {
-        printf("can not read zeta\n");
-    }
-
-    if (read_hdf5_dataset(file, "chi", H5T_NATIVE_DOUBLE, chi) < 0)
-    {
-        printf("can not read chi\n");
-    }
-}
-
-void read_hamiltonian(double *H)
-{
-    hid_t file = H5Fopen("../examples/thcmpo/data/water_h.h5", H5F_ACC_RDONLY, H5P_DEFAULT);
-    if (file < 0)
-    {
-        printf("'H5Fopen' failed\n");
-    }
-
-    if (read_hdf5_dataset(file, "H", H5T_NATIVE_DOUBLE, H) < 0)
-    {
-        printf("can not read H\n");
-    }
-}
-
-void read_kinetic(double *tkin)
-{
-    hid_t file = H5Fopen("../examples/thcmpo/data/water_t.h5", H5F_ACC_RDONLY, H5P_DEFAULT);
-    if (file < 0)
-    {
-        printf("'H5Fopen' failed\n");
-    }
-
-    if (read_hdf5_dataset(file, "T", H5T_NATIVE_DOUBLE, tkin) < 0)
-    {
-        printf("can not read H\n");
-    }
-}
 
 void water()
 {
@@ -64,20 +16,31 @@ void water()
     const double TOL = 1e-20;
     const long MAX_VDIM = LONG_MAX;
 
-    // TODO: Create one large hdf5 dataset instead of multiple ones.
-
-    // ζ, χ
-    struct dense_tensor zeta;
-    struct dense_tensor chi;
+    struct dense_tensor zeta; // ζ
     {
-        const long zeta_dim[2] = {N, N};
-        allocate_dense_tensor(CT_DOUBLE_REAL, 2, zeta_dim, &zeta);
-
-        const long chi_dim[2] = {N, L};
-        allocate_dense_tensor(CT_DOUBLE_REAL, 2, chi_dim, &chi);
-
-        read_data((double *)zeta.data, (double *)chi.data);
+        const long dim[2] = {N, N};
+        allocate_dense_tensor(CT_DOUBLE_REAL, 2, dim, &zeta);
     }
+
+    struct dense_tensor chi; // χ
+    {
+        const long dim[2] = {N, L};
+        allocate_dense_tensor(CT_DOUBLE_REAL, 2, dim, &chi);
+    }
+
+    struct dense_tensor H;  // H(water)
+    {
+        const long dim[2] = {16384, 16384};
+        allocate_dense_tensor(CT_DOUBLE_REAL, 2, dim, &H);
+    }
+
+    struct dense_tensor tkin; // t_{pq}
+    {
+        const long dim[2] = {L, L};
+        allocate_dense_tensor(CT_DOUBLE_REAL, 2, dim, &tkin);
+    }
+
+    read_water((double *)zeta.data, (double *)chi.data, (double *)H.data, (double *)tkin.data);
 
     // G_{nu, sigma}
     struct mpo **g;
@@ -87,22 +50,6 @@ void water()
         g[i] = ct_malloc(2 * sizeof(struct mpo));
     }
     construct_g_4d(chi, N, L, g);
-
-    // water hamiltonian 'H' 
-    struct dense_tensor H;
-    {
-        const long dim[2] = {16384, 16384};
-        allocate_dense_tensor(CT_DOUBLE_REAL, 2, dim, &H);
-        read_hamiltonian((double *)H.data);
-    }
-
-    // t_{pq}
-    struct dense_tensor tkin;
-    {
-        const long dim[2] = {L, L};
-        allocate_dense_tensor(CT_DOUBLE_REAL, 2, dim, &tkin);
-        read_kinetic((double *)tkin.data);
-    }
 
     // hartree fock state
     struct mps hfs;
@@ -126,7 +73,7 @@ void water()
     {
         const int i_ax = 1;
         struct timespec start, finish;
-        
+
         clock_gettime(CLOCK_MONOTONIC, &start);
         dense_tensor_multiply_axis(&H, i_ax, &hfs_vec, TENSOR_AXIS_RANGE_LEADING, &h_hfs);
         clock_gettime(CLOCK_MONOTONIC, &finish);
@@ -151,8 +98,11 @@ void water()
     struct mps v_psi;
     {
         struct timespec start, finish;
-        
-        construct_spin_zero_mps(L, spin_state, &v_psi);
+
+        // Initialize as '0' MPS.
+        const double alpha = 0.;
+        mps_deep_copy(&hfs, &v_psi);
+        scale_block_sparse_tensor(&alpha, &v_psi.a[0]);
 
         clock_gettime(CLOCK_MONOTONIC, &start);
         apply_thc(&hfs, g, zeta, N, TOL, MAX_VDIM, &v_psi);
@@ -163,16 +113,19 @@ void water()
         printf("V|psi> (SYN) [duration]=%fs\n", elapsed);
     }
 
-    struct mps v_psi_p;
+    struct mps v_psi_omp;
     {
         struct timespec start, finish;
 
-        construct_spin_zero_mps(L, spin_state, &v_psi_p);
+        // Initialize as '0' MPS.
+        const double alpha = 0.;
+        mps_deep_copy(&hfs, &v_psi_omp);
+        scale_block_sparse_tensor(&alpha, &v_psi_omp.a[0]);
 
         clock_gettime(CLOCK_MONOTONIC, &start);
-        apply_thc_omp(&hfs, g, zeta, N, TOL, MAX_VDIM, &v_psi_p);
+        apply_thc_omp(&hfs, g, zeta, N, TOL, MAX_VDIM, &v_psi_omp);
         clock_gettime(CLOCK_MONOTONIC, &finish);
-        
+
         double elapsed = (finish.tv_sec - start.tv_sec);
         elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
         printf("V|psi> (OMP) [duration]=%fs\n", elapsed);
@@ -184,7 +137,7 @@ void water()
 
     // t|ᴪ> + v|ᴪ>
     struct mps h_psi;
-    add_and_compress(&t_psi, &v_psi_p, TOL, MAX_VDIM, &h_psi);
+    add_and_compress(&t_psi, &v_psi_omp, TOL, MAX_VDIM, &h_psi);
 
     struct dense_tensor h_psi_vec;
     {
@@ -203,7 +156,7 @@ void water()
     // teardown
     delete_mps(&h_psi);
     delete_mps(&t_psi);
-    delete_mps(&v_psi_p);
+    delete_mps(&v_psi_omp);
     delete_mps(&v_psi);
     delete_mpo(&t_mpo);
     delete_dense_tensor(&h_hfs);
