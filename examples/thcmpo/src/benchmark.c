d9834c4d3fe9fa0516981ddf4408eec136296af6
Matthias Reumann <matthias.reumann@tum.de>
2024-12-11 10:02:52 +0100

    Add repeats to benchmarks

--- examples/thcmpo/src/benchmark.c
+++ examples/thcmpo/src/benchmark.c
@@ -12,7 +12,9 @@ void thc_benchmark_apply_thc_run(const long N, const long L, const double tol, c
 								 void (*read)(double*, double*, double*, double*),
 								 void (*apply_thcf)(const struct mps*, struct mpo**, const struct dense_tensor, const long, const double, const long, struct mps*),
 								 const struct mps* start) {
-	struct dense_tensor zeta; // ζ
+	const size_t REPEATS = 10;
+    
+    struct dense_tensor zeta; // ζ
 	{
 		const long dim[2] = {N, N};
 		allocate_dense_tensor(CT_DOUBLE_REAL, 2, dim, &zeta);
@@ -34,26 +36,30 @@ void thc_benchmark_apply_thc_run(const long N, const long L, const double tol, c
 	}
 	construct_g_4d(chi, N, L, g);
 
-	// v|ᴪ>
-	struct mps v_psi;
-	{
-		struct timespec t0, t1;
+    double sum_t = 0.;
+    for(size_t i = 0; i < REPEATS; i++) {
+        struct mps v_psi; // v|ᴪ>
+        {
+            struct timespec t0, t1;
 
-		// Initialize as '0' MPS.
-		const double alpha = 0.;
-		copy_mps(start, &v_psi);
-		scale_block_sparse_tensor(&alpha, &v_psi.a[0]);
+            // Initialize as '0' MPS.
+            const double alpha = 0.;
+            copy_mps(start, &v_psi);
+            scale_block_sparse_tensor(&alpha, &v_psi.a[0]);
 
-		clock_gettime(CLOCK_MONOTONIC, &t0);
-		apply_thcf(start, g, zeta, N, tol, max_vdim, &v_psi);
-		clock_gettime(CLOCK_MONOTONIC, &t1);
+            clock_gettime(CLOCK_MONOTONIC, &t0);
+            apply_thcf(start, g, zeta, N, tol, max_vdim, &v_psi);
+            clock_gettime(CLOCK_MONOTONIC, &t1);
 
-		double elapsed = (t1.tv_sec - t0.tv_sec);
-		elapsed += (t1.tv_nsec - t0.tv_nsec) / 1000000000.0;
-		printf("%f\n", elapsed);
-	}
+            double t = (t1.tv_sec - t0.tv_sec);
+            t += (t1.tv_nsec - t0.tv_nsec) / 1000000000.0;
+            sum_t += t;
+        }
+        delete_mps(&v_psi);
+    }
 
-	delete_mps(&v_psi);
+    printf("%f\n", sum_t / REPEATS);
+
 	// TODO: Free g
 	delete_dense_tensor(&chi);
 	delete_dense_tensor(&zeta);
